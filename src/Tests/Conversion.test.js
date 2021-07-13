682ed8baba6318930bea02f30409d1bc02abff64
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-13 19:33:55 -0400

    Readequado testes de validação de texto Formatado testes para padrão TypeScript

--- src/Tests/Conversion.test.js
+++ src/Tests/Conversion.test.js
@@ -1,5 +1,6 @@
-import {Conversion} from '../Functions/Utils/Conversions'
+import { Conversion } from '../Functions/Utils/Conversions'
+
 test("Testando metodo Conversion", () => {
-    expect(Conversion("12", "12", 5.2614, "dinheiro")).toBe("71.49")
-    expect(Conversion("12", "12", 5.2614, "cartao")).toBe("74.74")
+  expect(Conversion("12", "12", 5.2614, "dinheiro")).toBe("71.49");
+  expect(Conversion("12", "12", 5.2614, "cartao")).toBe("74.74");
 });
--- src/Tests/TextValidation.test.js
+++ src/Tests/TextValidation.test.js
@@ -1,22 +1,35 @@
-import { CheckIfInputIsValid, ReturnFormatted, ReturnPositive, ReturnValidPercentage} from '../Functions/Utils/TextValidation'
+import {
+  CheckIfInputIsValid,
+  ReturnFormattedNumber,
+  ReturnPositive,
+  ReturnValidPercentage,
+  ReturnFormattedPercentage
+} from "../Functions/Utils/TextValidation"
+
 test("Testando o método CheckIfInputIsValid", () => {
-    expect(CheckIfInputIsValid("","")).toBe(false)
-    expect(CheckIfInputIsValid("0","14.00")).toBe(false)
-    expect(CheckIfInputIsValid("0","0")).toBe(false)
-    expect(CheckIfInputIsValid("12.00","14.00")).toBe(true)
+  expect(CheckIfInputIsValid("", "")).toBe(false);
+  expect(CheckIfInputIsValid("0", "14.00")).toBe(false);
+  expect(CheckIfInputIsValid("0", "0")).toBe(false);
+  expect(CheckIfInputIsValid("12.00", "14.00")).toBe(true);
 });
 
-test("Testando o método ReturnFormatted", () => {
-    expect(ReturnFormatted("0.00")).toBe("")
-    expect(ReturnFormatted("2")).toBe("2.00")
+test("Testando o método ReturnFormattedNumber", () => {
+  expect(ReturnFormattedNumber("0")).toBe("");
+  expect(ReturnFormattedNumber("2")).toBe("2.00");
+});
+
+test("Testando o método ReturnFormattedPercentage", () => {
+  expect(ReturnFormattedPercentage("0")).toBe("0.00");
+  expect(ReturnFormattedPercentage("-12")).toBe("");
+  expect(ReturnFormattedPercentage("2")).toBe("2.00");
 });
 
 test("Testando o método ReturnPositive", () => {
-    expect(ReturnPositive("-12")).toBe(12)
-    expect(ReturnPositive(-12)).toBe(12)
+  expect(ReturnPositive("-12")).toBe(12);
+  expect(ReturnPositive(-12)).toBe(12);
 });
 
 test("Testando o método ReturnValidPercentage", () => {
-    expect(ReturnValidPercentage("10000")).toBe("9999")
-    expect(ReturnValidPercentage("-10000")).toBe("")
+  expect(ReturnValidPercentage("10000")).toBe("9999");
+  expect(ReturnValidPercentage("-10000")).toBe("");
 });
