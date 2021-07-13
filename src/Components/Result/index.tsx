afa2ee84ad0d17775f05908f39bad4d948cd5113
Caroline Oliveira <carolinevsboliveira@gmail.com>
2021-07-13 19:37:01 -0400

    Implementada a chamada do método ReturnPaymentMethodName Formatado o arquivo para evitar erros de case sensitive

--- src/Components/Result/index.tsx
+++ src/Components/Result/index.tsx
@@ -1,32 +1,55 @@
 //Components
-import {BackButton} from '../Buttons'
+import { BackButton } from '../Buttons'
 import MoneyInfo from '../MoneyInfo'
 
 //Style
-import {TitleText, ConvertText, InfoText} from './Style'
+import { TitleText, ConvertText, InfoText } from './Style'
 
 //Functions
-import {Conversion} from '../../Functions/Utils/Conversions'
+import { Conversion, ReturnPaymentMethodName } from '../../Functions/Utils/Conversions'
 
-interface iProps{
+interface iProps {
     dollarAmount: any,
     percentage: any,
-    dollarCurrentValue: Number, 
+    dollarCurrentValue: Number,
     paymentMethod: string,
-    backClicked:boolean,
-    backClickedHandler: Function 
+    backClicked: boolean,
+    backClickedHandler: Function
 }
-const Result = (props:iProps) => {
-    const convertedDollarValue = Conversion(props.dollarAmount, props.percentage, props.dollarCurrentValue, props.paymentMethod); 
+const Result = (props: iProps) => {
+    const convertedDollarValue = Conversion(
+        props.dollarAmount,
+        props.percentage,
+        props.dollarCurrentValue,
+        props.paymentMethod
+    );
     return (
         <>
-        <BackButton btnClicked={props.backClicked} btnkClickedHandler={props.backClickedHandler} btnName={"Voltar"}/>
-        <TitleText>O resultado do cálculo é:</TitleText>
-        <ConvertText>R$ {convertedDollarValue}</ConvertText>
-        <InfoText><b>Compra no {props.paymentMethod} e taxa de</b> {props.percentage} %</InfoText>
-        <InfoText><b>Cotação do dólar: </b> $1,00 = R$ {Number(props.dollarCurrentValue).toFixed(2)}</InfoText>
-        <MoneyInfo dollarAmount={props.dollarAmount} dollarCurrentValue={props.dollarCurrentValue} paymentMethod={props.paymentMethod} percentage={props.percentage} realConvertedValue={convertedDollarValue}/>
+            <BackButton
+                btnClicked={props.backClicked}
+                btnClickedHandler={props.backClickedHandler}
+                btnName={"Voltar"}
+            />
+            <TitleText>O resultado do cálculo é:</TitleText>
+            <ConvertText>R$ {convertedDollarValue}</ConvertText>
+            <InfoText>
+                <b>
+                    Compra no {ReturnPaymentMethodName(props.paymentMethod)} e taxa de
+                </b>{" "}
+                {props.percentage} %
+            </InfoText>
+            <InfoText>
+                <b>Cotação do dólar: </b> $1,00 = R${" "}
+                {Number(props.dollarCurrentValue).toFixed(2)}
+            </InfoText>
+            <MoneyInfo
+                dollarAmount={props.dollarAmount}
+                dollarCurrentValue={props.dollarCurrentValue}
+                paymentMethod={props.paymentMethod}
+                percentage={props.percentage}
+                realConvertedValue={convertedDollarValue}
+            />
         </>
-      );
-  }
-  export default Result
+    );
+};
+export default Result
